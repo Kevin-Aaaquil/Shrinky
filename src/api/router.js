9bd8fc7a18c30b473c8bb0b2e7ef9cda0e0e1d85
Kevin-Aaaquil <eshansingh2409@gmail.com>
2021-06-05 17:47:14 +0530

    ejs added

--- src/api/router.js
+++ src/api/router.js
@@ -15,12 +15,16 @@ router.get('/:code', (req, res) => {
         })
 })
 
+router.get('/',(req,res)=>{
+res.render('index.ejs')
+})
 
 
 router.post('/create/link', (req, res) => {
     controller.createCode(req.body.link.toString())
         .then((code) => {
-            res.status(203).json({ link: `localhost:3000/${code}` })
+           // res.status(203).json({ link: `localhost:3000/${code}` })
+           res.status(203).render('result.ejs',{link:`www.shrinkky.herokuapp.com/${code}`})
         })
         .catch((err) => {
             res.status(404).json({ code: err.code, message: err.message, success: false })
--- src/index.js
+++ src/index.js
@@ -14,9 +14,9 @@ DB().catch(err=>console.log(err.message));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.get('/hell',(req,res)=>{
-    console.log("hell");
-    res.send("jgg")
+app.set('view-engine','ejs')
+app.get('/',(req,res)=>{
+    res.render('index.ejs');
 })
 app.use(routes);
 
