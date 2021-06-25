067f010bac00f7896d28c2de875d76c56d20f34c
Kevin-Aaaquil <eshansingh2409@gmail.com>
2021-06-25 11:06:23 +0530

    test

--- src/api/router.js
+++ src/api/router.js
@@ -29,10 +29,10 @@ router.post('/create/link', (req, res) => {
     controller.createCode(req.body.link.toString())
         .then((code) => {
            // res.status(203).json({ link: `localhost:3000/${code}` })
-           link = code;
-           res.redirect('/result')
+        //    link = code;
+        //    res.redirect('/result')
            
-          // res.status(203).render('result.ejs',{link:`shrinkky.herokuapp.com/${code}`})
+          res.status(203).render('result.ejs',{link:`shrinkky.herokuapp.com/${code}`})
         })
         .catch((err) => {
             res.status(404).json({ code: err.code, message: err.message, success: false })
@@ -49,15 +49,15 @@ router.delete(`/delete/${process.env.password}`,async (req,res)=>{
     }
 })
 
-router.get('/result',(req,res)=>{
-if(link){
-    res.status(203).render('result.ejs',{link:`shrinkky.herokuapp.com/${link}`})
-    link = 0;
-}
-else{
-    res.redirect('/')
-}
-})
+// router.get('/result',(req,res)=>{
+// if(link){
+//     res.status(203).render('result.ejs',{link:`shrinkky.herokuapp.com/${link}`})
+//     link = 0;
+// }
+// else{
+//     res.redirect('/')
+// }
+// })
 
 
 
