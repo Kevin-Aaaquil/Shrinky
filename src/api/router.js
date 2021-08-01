f4a08d9a06d0ce7bc4f3b1fef33169111cf4889d
Kevin-Aaaquil <eshansingh2409@gmail.com>
2021-08-01 20:18:03 +0530

    rollback

--- src/api/router.js
+++ src/api/router.js
@@ -6,10 +6,10 @@ const controller = require('./controller');
 const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 
-router.post('/fetch/link/', (req, res) => {
-    controller.getUrl(req.body.code.toString())
+router.get('/:code', (req, res) => {
+    controller.getUrl(req.params.code.toString())
         .then((link) => {
-            res.status(203).json({link : link})
+            res.redirect(link);
         })
         .catch((err) => {
             res.status(404).json({ code: err.code, message: err.message, success: false })
@@ -21,21 +21,14 @@ router.post('/fetch/link/', (req, res) => {
 router.post('/create/link', (req, res) => {
     controller.createCode(req.body.link.toString())
         .then((code) => {
-            res.status(203).json({ link: `http://localhost:3000/${code}` })        // this link is frontend link
+           // res.status(203).json({ link: `localhost:3000/${code}` })
+           res.status(203).json({link:`https://shrinkky.herokuapp.com/${code}`})
+          // res.status(203).render('result.ejs',{link:`shrinkky.herokuapp.com/${code}`})
         })
         .catch((err) => {
             res.status(404).json({ code: err.code, message: err.message, success: false })
         })
 
-})
-
-// get to backend will send you to frontend
-router.get('/',(req,res)=>{
-    res.redirect('http://localhost:3000')
-})
-
-router.get('/:code',(req,res)=>{
-    res.redirect(`http://localhost:3000/${req.params.code}`)
 })
 
 router.delete(`/delete/${process.env.password}`,async (req,res)=>{
